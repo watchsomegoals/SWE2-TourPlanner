ff45fcf432a45e55d6b5d15317f54d92fceef603
Valentin Caraba <if18b122@technikum-wien.at>
2021-03-09 19:44:27 +0100

    First commit with basic interface

---
+++ SWE2TourPlanner/ExecuteCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace SWE2TourPlanner
+{
+    public class ExecuteCommand : ICommand
+    {
+        private readonly MainViewModel mainViewModel;
+
+        public ExecuteCommand(MainViewModel mainViewModel)
+        {
+            this.mainViewModel = mainViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            float price = 0;
+            mainViewModel.CustOrder = "A pizza with";
+            if (mainViewModel.IsCheckedBoxSalami)
+            {
+                mainViewModel.CustOrder += " salami";
+                price += 2;
+            }
+            if (mainViewModel.IsCheckedBoxProsciutto)
+            {
+                mainViewModel.CustOrder += " prosciutto";
+                price += 3;
+            }
+            if (mainViewModel.IsCheckedBoxCorn)
+            {
+                mainViewModel.CustOrder += " corn";
+                price += 1;
+            }
+            if (mainViewModel.IsCheckedBoxBacon)
+            {
+                mainViewModel.CustOrder += " bacon";
+                price += 3;
+            }
+            if (mainViewModel.IsCheckedBoxMushrooms)
+            {
+                mainViewModel.CustOrder += " mushrooms";
+                price += 2;
+            }
+            mainViewModel.Price = price + (price * mainViewModel.Tax/100);
+        }
+    }
+}
---
+++ SWE2TourPlanner/MainViewModel.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace SWE2TourPlanner
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private float tax;
+        private float price;
+        private string custOrder;
+        private bool checkedSalami;
+        private bool checkedProsciutto;
+        private bool checkedCorn;
+        private bool checkedBacon;
+        private bool checkedMushrooms;
+        public ICommand ExecuteCommand { get; }
+        public MainViewModel()
+        {
+            this.ExecuteCommand = new ExecuteCommand(this);
+        }
+
+        public float Tax
+        {
+            get
+            {
+                Debug.Print("read tax percentage");
+                return tax;
+            }
+            set
+            {
+                if(Tax != value)
+                {
+                    Debug.Print("set tax value");
+                    tax = value;
+
+                    Debug.Print("fire propertyChanged: Tax");
+                    OnPropertyChanged(nameof(Tax));
+                }
+            }
+        }
+
+        public float Price
+        {
+            get
+            {
+                Debug.Print("read price");
+                return price;
+            }
+            set
+            {
+                if (Price != value)
+                {
+                    Debug.Print("set price value");
+                    price = value;
+
+                    Debug.Print("fire propertyChanged: Price");
+                    OnPropertyChanged(nameof(Price));
+                }
+            }
+        }
+
+        public string CustOrder
+        {
+            get
+            {
+                Debug.Print("read customer order");
+                return custOrder;
+            }
+            set
+            {
+                if(CustOrder != value)
+                {
+                    Debug.Print("set customer order value");
+                    custOrder = value;
+
+                    Debug.Print("fire propertyChanged: Customer Order");
+                    OnPropertyChanged(nameof(CustOrder));
+                }
+            }
+        }
+
+        public bool IsCheckedBoxSalami
+        {
+            get { return checkedSalami; }
+            set
+            {
+                if(IsCheckedBoxSalami != value)
+                {
+                    checkedSalami = value;
+                    OnPropertyChanged(nameof(IsCheckedBoxSalami));
+                }
+            }
+        }
+
+        public bool IsCheckedBoxProsciutto
+        {
+            get { return checkedProsciutto; }
+            set
+            {
+                if(IsCheckedBoxProsciutto != value)
+                {
+                    checkedProsciutto = value;
+                    OnPropertyChanged(nameof(IsCheckedBoxProsciutto));
+                }
+            }
+        }
+
+        public bool IsCheckedBoxCorn
+        {
+            get { return checkedCorn; }
+            set
+            {
+                if (IsCheckedBoxCorn != value)
+                {
+                    checkedCorn = value;
+                    OnPropertyChanged(nameof(IsCheckedBoxCorn));
+                }
+            }
+        }
+
+        public bool IsCheckedBoxBacon
+        {
+            get { return checkedBacon; }
+            set
+            {
+                if (IsCheckedBoxBacon != value)
+                {
+                    checkedBacon = value;
+                    OnPropertyChanged(nameof(IsCheckedBoxBacon));
+                }
+            }
+        }
+
+        public bool IsCheckedBoxMushrooms
+        {
+            get { return checkedMushrooms; }
+            set
+            {
+                if (IsCheckedBoxMushrooms != value)
+                {
+                    checkedMushrooms = value;
+                    OnPropertyChanged(nameof(IsCheckedBoxMushrooms));
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            Debug.Print($"propertyChanged \"{propertyName}\"");
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
